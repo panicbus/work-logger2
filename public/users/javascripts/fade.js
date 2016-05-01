30197b4ce331558cfc5d362dd7a68030114a9140
panicbus <elementalair@gmail.com>
2016-05-01 13:05:25 -0700

    unnecessary public js file

--- public/users/javascripts/fade.js
+++ public/users/javascripts/fade.js
@@ -2,13 +2,10 @@ $(document).ready(function(){
 	wait();
 });
 
-
 function wait(){
-	// alert('wait function')
 	setTimeout(flash_fade, 2000);
 }
 
 function flash_fade(){
-	// alert('flash_fade')
 	$('.flash-container').fadeOut('fast');
 }
