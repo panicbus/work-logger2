a981a670e0118f9e449cb0763a1185470cfc09b1
panicbus <elementalair@gmail.com>
2016-05-27 12:05:48 -0700

    removinh psw trace

--- config/index.js
+++ config/index.js
@@ -5,6 +5,6 @@ module.exports = {
 	getDbConnectionString : function(){
 		// this is the connection path from mlabs database w the uname/psw from config.json
 		return 'mongodb://' + configValues.uname + ':' + configValues.psw + '@ds015909.mlab.com:15909/worklogger';
-		// return 'mongodb://nicocrisafulli:3kimana5@ds015909.mlab.com:15909/worklogger';
+		// return 'mongodb://username:password@ds015909.mlab.com:15909/worklogger';
 	}
 }
